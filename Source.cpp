1f111dbbc2cfdc11e3e6809e48bab1969c011d58
Pirate021 <rezak04@gmail.com>
2016-11-30 13:38:37 +0330

    Array Creation
    Creating Array and assign the size taken and random numbers (between 0
    to 10000)

--- Source.cpp
+++ Source.cpp
@@ -4,13 +4,34 @@
 > Sort kinds: Selection, Insertion, Bubble, Merge, Quick
 > Range of the index amounts is between 0 and 10000
 > Array Sizes starts from 10 to 1000
+> We will use "unsigned short int" to create arrays because the required range is in this kind (uses less storage and less to process)
 > The purpose of this program is to assign the best sort algorithm for an specific array size 
 
 */
 
 #include<iostream>
+#include<time.h>
 
 using namespace std;
+
+
+struct array_
+{
+	unsigned short int * set; //The Array that wants to get sorted
+
+public:
+	void create_array(unsigned short int size)
+	{
+		srand(time(NULL));
+		unsigned short int random; //The generated integer will be saved in this
+		for (unsigned short int i = 0; i < size; i++)
+		{
+			random = time(NULL) % 10000;
+			set[i] = random;
+		}
+
+	}
+};
 
 int main()
 {
