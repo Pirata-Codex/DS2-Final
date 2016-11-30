9fa41f592c8bdc8e32849e112b6015be705aae69
Pirate021 <rezak04@gmail.com>
2016-11-30 13:12:36 +0330

    Creating Project

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+/*
+
+> Sorting Project by Reza Khaleghi
+> Sort kinds: Selection, Insertion, Bubble, Merge, Quick
+> Range of the index amounts is between 0 and 10000
+> Array Sizes starts from 10 to 1000
+> The purpose of this program is to assign the best sort algorithm for an specific array size 
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
