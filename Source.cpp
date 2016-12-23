0d175ff7f7fd48c5a4e7fb5d7a69f3021ffa2bcb
Pirate021 <rezak04@gmail.com>
2016-12-23 11:37:31 +0330

    Quick sort
    Quick sort has been added and the comparisons, swaps and time is a go!

--- Source.cpp
+++ Source.cpp
@@ -53,7 +53,7 @@ public:
 	void create_array(unsigned short int);
 	void set_size(unsigned short int);
 
-	//Sorting algorithms
+	//:::::::::::::::::::::::::::::::::::::::::::::::::::::::: ALGORITHMS :::::::::::::::::::::::::::::::::::::::::::::::::::::
 	void bubble_sorting();
 	void insertion_sorting();
 	void selection_sorting();
@@ -66,6 +66,14 @@ public:
 	//Merge sort functions
 	void merge(unsigned short int *, unsigned short int, unsigned short int, unsigned short int);
 	void mergesort(unsigned short int *, unsigned short int, unsigned short int);
+
+	//Quick sort functions
+	void swap(unsigned short int *, unsigned short int *);
+	int partition(unsigned short int arr[], unsigned short int low, unsigned short int high);
+	void quicksort(unsigned short int *, unsigned short int, unsigned short int);
+	//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	void sort_array();
 
 };
 
@@ -334,6 +342,74 @@ void array_::mergesort(unsigned short int * arr, unsigned short int l, unsigned 
 	}
 
 }
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::: QUICK SORT :::::::::::::::::::::::::::::::::::::::::::::::::::::
+void array_::swap(unsigned short int * a, unsigned short int * b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+	Quick_swap++;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+int array_::partition(unsigned short int arr[], unsigned short int low, unsigned short int high)
+{
+	unsigned short int pivot = arr[high];    // pivot
+	unsigned short int i = (low - 1);  // Index of smaller element
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than or
+		// equal to pivot
+		if (arr[j] <= pivot)
+		{
+			Quick_comp++;
+			i++;    // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+		Quick_comp++;
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low  --> Starting index,
+high  --> Ending index */
+void array_::quicksort(unsigned short int * arr, unsigned short int low, unsigned short int high)
+{
+	Quick_comp = Quick_swap = 0;
+
+	double temp_time = clock_t();
+	if (low < high)
+	{
+		Quick_comp++;
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		unsigned short int pi = partition(arr, low, high);
+
+		// Separately sort elements before
+		// partition and after partition
+		quicksort(arr, low, pi - 1);
+		quicksort(arr, pi + 1, high);
+	}
+	Quick_time = (clock_t() - temp_time);
+}
+
+void array_::sort_array()
+{
+	create_array(size);
+
+}
+
+
 
 int main()
 {
